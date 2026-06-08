30fd0118052c3b1689981d1641ce37ec703a7184
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-08 17:11:17 +0200

    Start new project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { FullscreenMapPage } from './pages/fullscreen-map-page/fullscreen-map-page';
+import { MarkersPage } from './pages/markers-page/markers-page';
+import { HousesPage } from './pages/houses-page/houses-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'fullscreen',
+    component: FullscreenMapPage,
+    title: 'Fullscreen Map'
+  },
+  {
+    path: 'markers',
+    component: MarkersPage,
+    title: 'Markers'
+  },
+  {
+    path: 'houses',
+    component: HousesPage,
+    title: 'Houses'
+  },
+  {
+    path: '**',
+    redirectTo: 'fullscreen'
+  }
+];
