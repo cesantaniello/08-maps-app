ee9fd6c058be2227d07a3fb3799fb542700a9279
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-11 16:54:16 +0200

    Remove markers

--- src/app/pages/markers-page/markers-page.ts
+++ src/app/pages/markers-page/markers-page.ts
@@ -63,5 +63,13 @@ export class MarkersPage implements AfterViewInit{
 
     this.map()?.flyTo({center: lngLat});
   }
+
+  deleteMarker(marker: Marker){
+    if(!this.map()) return;
+    const map = this.map()!;
+
+    marker.mapboxMarker.remove();
+    this.markers.set(this.markers().filter((m) => m.id != marker.id));
+  }
 }
 
