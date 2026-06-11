1f5b127bbf85ca1790e1617b5441f8f6c76526e6
Carlos Eduardo Santaniello Lozano <carlos-eduardo.santaniello-lozano@capgemini.com>
2026-06-11 10:55:46 +0200

    Create dynamic markers

--- src/app/pages/markers-page/markers-page.ts
+++ src/app/pages/markers-page/markers-page.ts
@@ -1,8 +1,14 @@
 import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import { environment } from '../../../environments/environment';
+import { v4 as UUIDv4 } from 'uuid'
 
 mapboxgl.accessToken = environment.mapboxKey;
+
+interface Marker {
+  id: string;
+  mapboxMarker: mapboxgl.Marker;
+}
 
 @Component({
   selector: 'app-markers-page',
@@ -12,7 +18,7 @@ mapboxgl.accessToken = environment.mapboxKey;
 export class MarkersPage implements AfterViewInit{
   divElement = viewChild<ElementRef>('map');
   map = signal(<mapboxgl.Map | null>null);
-
+  markers = signal<Marker[]>([]);
 
   async ngAfterViewInit(){
     const element = this.divElement()?.nativeElement;
@@ -24,13 +30,31 @@ export class MarkersPage implements AfterViewInit{
       center: [-0.38, 39.47],
       zoom: 14,
     });
-
+/*
     const marker = new mapboxgl.Marker({draggable:false, color:'orange'}).setLngLat([-0.38, 39.47]).addTo(map);
 
     marker.on('dragend', (event) => {})
+*/
     this.mapListeners(map);
   }
+  mapListeners(map: mapboxgl.Map){
+    map.on('click', (event) => this.mapClick(event));
+    this.map.set(map);
+  }
 
-  mapListeners(map: mapboxgl.Map){}
+  mapClick(event: mapboxgl.MapMouseEvent){
+    if(!this.map()) return;
+
+    const map = this.map()!;
+    const coords = event.lngLat;
+    const color = '#xxxxxx'.replace(/x/g, (y) => ((Math.random()*16) | 0).toString(16));
+
+    const mapboxMarker = new mapboxgl.Marker({color:color}).setLngLat(coords).addTo(map);
+
+    const newMarker: Marker = {id: UUIDv4(), mapboxMarker: mapboxMarker}
+
+    this.markers.update((markers)=> [newMarker, ...markers])
+    console.log(this.markers())
+  }
 }
 
